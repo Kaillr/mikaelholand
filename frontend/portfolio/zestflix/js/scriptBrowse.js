6b7b2e9e48f4688884b5ac2adf6f019c6cd39557
Kaillr <125704518+Kaillr@users.noreply.github.com>
2024-10-11 10:10:55 +0200

    chore: Remove Zestflix from repo

--- frontend/portfolio/zestflix/js/scriptBrowse.js
+++
@@ -1,7 +0,0 @@
-// Add scroll event listener to window
-window.addEventListener('scroll', () => {
-    // Get the header element
-    const header = document.getElementById('headerSolid');
-    // Toggle 'show' class based on page Y offset
-    header.classList.toggle('show', window.pageYOffset > 0);
-});
