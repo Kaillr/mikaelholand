11d3ccc77d4810c6cff5ebdf3e01d8e4658ccd06
Kaillr <125704518+Kaillr@users.noreply.github.com>
2024-10-14 11:03:47 +0200

    chore: moved app.js to backend directory

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,18 @@ const app = express();
 const port = 3001;
 
 // Serve the frontend folder as static files
-app.use(express.static(path.join(__dirname, "frontend")));
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "index.html"));
 });
 
 app.get("/aboutme", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "aboutme.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "aboutme.html"));
 });
 
 app.get("/portfolio", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "portfolio.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "portfolio.html"));
 });
 
 
